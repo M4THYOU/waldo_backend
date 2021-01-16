acf14d8b8a719233e82b8c541dbbb26aff7fe83e
Matt Wolfe <m5wolfe@uwaterloo.ca>
2021-01-16 11:23:38 -0500

    hide secret key

--- waldo/settings.py
+++ waldo/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'r^k3^lrl$e^n(%h=ex_e+%(%u=eyrqrf9p)*=bbs484mk0q%y9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
